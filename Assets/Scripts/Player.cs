d37e3a54b9ed6848583436fbb7129051f1029a6d
Keyrune <spyro-99@mail.ru>
2021-10-04 17:42:04 +0300

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+        }
+
+    }
+}
