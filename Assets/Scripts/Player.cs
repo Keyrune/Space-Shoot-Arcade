1007e052895a2b1565b487d4f3e5bd254f788951
Keyrune <spyro-99@mail.ru>
2021-10-04 20:04:27 +0300

    basic ship

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float moveX;
+    public float offsetY = -3.5f;
+
     void Update()
     {
         if (Input.touchCount > 0)
         {
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
             touchPosition.z = 0f;
+            
             transform.position = touchPosition;
+
         }
 
     }
