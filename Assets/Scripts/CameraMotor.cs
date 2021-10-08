9e9e1298f68b50007dad5dc5bd2b918ebd7df20f
Keyrune <spyro-99@mail.ru>
2021-10-08 17:24:37 +0300

    camera follow

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMotor : MonoBehaviour
-{
+{   
+    [SerializeField] private Transform player;
+    public float cameraBounds = 0.2f;
     public float speed = 1f;
+
+
     void Update()
     {
-        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        moveCamera();
+    }
+
+    private void moveCamera()
+    {
+        Vector3 newPosition;
+
+        newPosition.x = player.position.x * cameraBounds;
+        newPosition.y = transform.position.y + speed * Time.deltaTime; 
+        newPosition.z = -10f;
+
+        transform.position = newPosition;
     }
 }
